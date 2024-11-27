375299f12e3e0e24cc68f0d33369a62369c223ab
Andy-Tho <andy1997.ak@gmail.com>
2024-11-27 08:42:42 +0100

    edited output for logarithmischefunktionen.php

--- logarithmischefunktionen.php
+++ logarithmischefunktionen.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kontrollstrukturen</title>
+    <style>
+        p{
+            font-size: small;
+            line-height: 0.5px;
+        }
+    </style>
 </head>
 <body>
     <h1>Logarithmische Funktionen</h1>
     <?php
     for($i = 1; $i <= 100; $i++){
         $log = log($i);
-        for($j = 1; $j <= round($log * 10); $j++){
-            echo '*';
-        }
-        echo '<br>';
+        echo '<p>' . str_repeat("*", round($log * 10)) . '</p>';
     }
     ?>
 </body>
